dc6d6a22bd788048b618ca5b98e824a2e7f38538
DBK112 <duongbaokhanh123tv@gmail.com>
2025-12-14 19:46:16 +0700

    login feature

--- pet.sql
+++ pet.sql
@@ -2,12 +2,6 @@ drop DATABASE IF EXISTS pet_care_db;
 create DATABASE pet_care_db;
 use pet_care_db;
 
-CREATE TABLE accounts (
-    accountID INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL -- Nên để dài để lưu mã hóa hash
-);
-
 CREATE TABLE services (
     serviceID INT PRIMARY KEY AUTO_INCREMENT,
     serviceName VARCHAR(100) NOT NULL,
@@ -30,17 +24,19 @@ CREATE TABLE work_schedules (
     isAvailable BOOLEAN DEFAULT 1,
     FOREIGN KEY (employeeID) REFERENCES employees(employeeID) ON DELETE CASCADE
 );
-CREATE TABLE customers (
-    customerID INT PRIMARY KEY AUTO_INCREMENT,
-    accountID INT, -- Một tài khoản quản lý thông tin khách hàng
-    customerName VARCHAR(100) NOT NULL,
+CREATE TABLE users (
+    userID INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     contactNumber VARCHAR(20),
-    email VARCHAR(100),
-    FOREIGN KEY (accountID) REFERENCES accounts(accountID) ON DELETE CASCADE
+    address TEXT,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 CREATE TABLE pets (
     petID INT PRIMARY KEY AUTO_INCREMENT,
-    customerID INT NOT NULL,
+    userID INT NOT NULL,
     petName VARCHAR(50) NOT NULL,
     species VARCHAR(50), -- Chó, Mèo...
     breed VARCHAR(50),   -- Giống
@@ -48,18 +44,18 @@ CREATE TABLE pets (
     age INT,
     petImage VARCHAR(255), -- Lưu đường dẫn ảnh
     medicalHistory TEXT,
-    FOREIGN KEY (customerID) REFERENCES customers(customerID) ON DELETE CASCADE
+    FOREIGN KEY (userID) REFERENCES users(userID) ON DELETE CASCADE
 );
 CREATE TABLE appointments (
     appointmentID INT PRIMARY KEY AUTO_INCREMENT,
-    customerID INT NOT NULL,
+    userID INT NOT NULL,
     petID INT NOT NULL,
     employeeID INT,
     serviceID INT NOT NULL,
     appointmentDate DATETIME, -- Thời gian hẹn (quan trọng dù hình không vẽ)
     note TEXT,
     status VARCHAR(50) DEFAULT 'Pending', -- Pending, Confirmed, Completed, Cancelled
-    FOREIGN KEY (customerID) REFERENCES customers(customerID),
+    FOREIGN KEY (userID) REFERENCES users(userID),
     FOREIGN KEY (petID) REFERENCES pets(petID),
     FOREIGN KEY (employeeID) REFERENCES employees(employeeID),
     FOREIGN KEY (serviceID) REFERENCES services(serviceID)
